2a1921d991fc372853c2b733eed18e2bbdda5ec2
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2025-08-04 20:24:37 +0700

    feat: integrate product & sales remote data

--- lib/helpers/injector.dart
+++ lib/helpers/injector.dart
@@ -2,6 +2,8 @@ import 'package:flutter_batch_8_project/data/local_storage/auth_local_storage.da
 import 'package:flutter_batch_8_project/data/local_storage/theme_local_storage.dart';
 import 'package:flutter_batch_8_project/data/remote_data/auth_remote_data.dart';
 import 'package:flutter_batch_8_project/data/remote_data/network_service/network_service.dart';
+import 'package:flutter_batch_8_project/data/remote_data/product_remote_data.dart';
+import 'package:flutter_batch_8_project/data/remote_data/sales_remote_data.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
@@ -32,5 +34,6 @@ void networkInjector() {
   );
   getIt.registerSingleton(networkService);
   getIt.registerSingleton<AuthRemoteData>(AuthRemoteDataImpl(networkService));
-  // TODO: create singleton
+  getIt.registerSingleton<ProductRemoteData>(ProductRemoteDataImpl(networkService));
+  getIt.registerSingleton<SalesRemoteData>(SalesRemoteDataImpl(networkService));
 }
